b036a6075cdbc65fe823a8bdf446ae7a8ad69d11
ArterOhm <Aef117722@gmail.com>
2023-10-09 18:23:38 +0700

    Menu Hamburger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,36 @@
 import { NavLink } from 'react-router-dom'
 import classes from './Navbar.module.css'
+import { useState } from 'react'
+
 
 const Navbar =() =>{
+  const[menuOpen ,setMenuOpen] =useState<boolean>(false)
+  
     return (
-        <div className={classes.navbar}>
-            <div className={classes.menubar}>
+        <nav className={classes.navbar}>
+            <div className={classes.menubarL}>
             <NavLink className={ classes.inactive} to="/">
               LOGO
             </NavLink>
             </div>
-            <div className={classes.menubar}>
-            <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inactive)} to="/create">
+            <div  className={classes.menubarHam } onClick={()=>{return (setMenuOpen(!menuOpen) ,console.log(menuOpen))}} >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={menuOpen? classes.menubarR:  classes.menubarOpen}>
+            <NavLink className={classes.HomeMeun}  to="/">
+              Home
+            </NavLink>
+            <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inactive)} to="/create" >
               Create
             </NavLink>
-            <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inactive)} to="/profile">
+            <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inactive)} to="/profile" >
               Profile
             </NavLink>
-            <NavLink className={classes.login} to="/login">Login</NavLink>
+            <NavLink className={classes.login} to="/login"  >Login</NavLink >
             </div>
-        </div>
+        </nav>
     )
 }
 
